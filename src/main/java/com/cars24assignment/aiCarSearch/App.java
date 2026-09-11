34cf9a270dd21c5aaca652c32acdaee6eb3f8f31
Kalash Sharma <kalash.strt@gmail.com>
2026-09-11 21:53:21 +0530

    chore: application properties yaml files added for profiles

--- src/main/java/com/cars24assignment/aiCarSearch/App.java
+++ src/main/java/com/cars24assignment/aiCarSearch/App.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AiCarSearchApplication {
+public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AiCarSearchApplication.class, args);
+		SpringApplication.run(App.class, args);
 	}
 
 }
