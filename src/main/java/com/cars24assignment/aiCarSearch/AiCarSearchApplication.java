3e0716f9414a7bd39650184caebfb65575c1b426
Kalash Sharma <kalash.strt@gmail.com>
2026-09-11 21:44:37 +0530

    chore: setup new spring boot app

---
+++ src/main/java/com/cars24assignment/aiCarSearch/AiCarSearchApplication.java
@@ -0,0 +1,13 @@
+package com.cars24assignment.aiCarSearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCarSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiCarSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cars24assignment/aiCarSearch/AiCarSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cars24assignment.aiCarSearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCarSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
